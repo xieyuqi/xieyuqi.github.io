94c89878c4fdb49d175a0b646bb01b443776ff4b
xyq123 <xyq123@gmail.com>
2015-01-06 22:38:00 +0800

    change setting

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,8 +1,8 @@
-var disqus_shortname = 'hooloodemo';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://hooloo.github.io';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'hooloo';						//Your username of GitHub.
-var repos = 'hooloo.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'Hooloo';						//Name of your blog.
+var disqus_shortname = 'xieyuqi';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://xieyuqi.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'xieyuqi';						//Your username of GitHub.
+var repos = 'xieyuqi.github.io';					//Name of repository that contains your blog.
+var sitetitle = '树懒先生';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
